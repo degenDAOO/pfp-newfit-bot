538ca495a966c6e561dd5d5beaac34f6782219f8
jsblair9 <james@lionsmouth.digital>
2022-07-21 12:12:43 -0500

    ⭐️ total refactor to the gib command

--- main.py
+++ main.py
@@ -10,44 +10,79 @@ from dotenv import load_dotenv
 import PIL
 from PIL import Image
 
-# discord bot
+# discord bot prefix
 bot = commands.Bot(command_prefix="!")
 
-# Opening JSON file with pfps. Add your open file and update it to match the name here
-# data = open('backup.json', )
-
-# Folder locations for clean pfps, completed pfps, and outfits
-
-save_img_folder = 'dressed_pfps/'
-outfits_folder = 'outfits/beer/'
-pfp_folder = 'clean_pfps/'
-
-save_bussin_img_folder = 'bussin/dressed_pfps/'
-outfits_bussin_folder = 'outfits/bussin/'
-
-outfits_dtp_bussin_folder = 'dtp/outfits/bussin/'
-
-save_dtp_img_folder = 'dtp/dressed_pfps/'
-outfits_dtp_folder = 'dtp/outfits/beer/'
-pfp_dtp_folder = 'dtp/clean_pfps/'
-
-save_egg_img_folder = 'egg/dressed_pfps/'
-outfits_egg_folder = 'egg/outfits/beer/'
-pfp_egg_folder = 'egg/clean_pfps/'
-
-save_jersey_img_folder = 'jersey/dressed_pfps/'
-outfits_jersey_folder = 'jersey/outfits/beer/'
-pfp_jersey_folder = 'jersey/clean_pfps/'
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 
-# list of the various outfits you want to offer. these should match the filename on the outfit pngs
+# # Bot command functions overview
+# 0. inputs: collection, pfp_id, outfit, variant
+# 1. Get base image
+# 2. Download base image to clean_pfp dir 
+# 3. Combine images & save to dressed_pfp dir
+# 4. Send dressed_pfp image to discord
 
-outfits = ["background", "clean", "black", "blue", "brown", "gold", "green", "purple", "red", "white"]
+# Current Campaigns
+# - Bussin
+# - Cap
+# - Beer
+#
+# Archive Campaigns
+# - Seven
 
-# Search for the pfp id in the JSON dictionary and return the image URL associated with that id. You'll need to update the keys to match what's in your JSON delattr
+# Collections available
+collections = ['ape', 'dtp', 'egg']
 
+# Functions
 
-# Downloads the pfp from the image URL and saves it in a directory
+def get_daa_image(pfp_id):
+  url = ('https://degenape.nyc3.digitaloceanspaces.com/apes/no-head-traits/' + str(pfp_id) + '.png')
+  save_image_file_path = ('collections/ape/clean_pfps/' + str(pfp_id) + '.png') 
+
+  download_image(url, save_image_file_path)
+
+def get_collection_image(collection, pfp_id):
+  search_value = {
+    'dtp': {
+      'project_id': 'degentrashpandas',
+      'search_value': 'Degen Trash Panda #',
+    },
+    'egg': {
+      'project_id': 'degenerateapekindergarten',
+      'search_value': 'Degen Egg #',
+    },
+  }
+
+  payload = json.dumps({
+        'condition': {
+            'project_ids': [
+                {
+                    'project_id': search_value[collection]['project_id']
+                }
+            ],
+            'name': {
+                'operation': 'EXACT', 
+                'value': search_value[collection]['search_value'] + str(pfp_id)
+                },
+        }
+    })
+  headers = {
+      'Authorization': os.environ['HYPER_TOKEN'],
+      'Content-Type': 'application/json'
+  }
+  r = requests.post('https://beta.api.solanalysis.com/rest/get-market-place-snapshots', headers=headers, data=payload)
+  
+  url = r.json()['market_place_snapshots'][0]['meta_data_img']
+
+  save_image_file_path = ('collections/' + collection + '/clean_pfps/' + str(pfp_id) + '.png')
+
+  download_image(url, save_image_file_path)
 
 def download_image(url, image_file_path):
     r = requests.get(url, timeout=4.0)
@@ -57,259 +92,74 @@ def download_image(url, image_file_path):
     with Image.open(io.BytesIO(r.content)) as im:
         im.save(image_file_path)
 
-# Combines the pfp image with a transparent png of the attribute and saves it to an output directory
+def combine_images(collection, outfit, fit, pfp_id):
+  save_file_path = ('collections/' + collection + '/dressed_pfps/' + str(pfp_id) + '_' + outfit + '_' + fit + '.png')
 
-def get_dressed(fit, pfp_id):
-    url = ('https://degenape.nyc3.digitaloceanspaces.com/apes/no-head-traits/' + str(pfp_id) + '.png')
-    download_image(url, pfp_folder + str(pfp_id) + '.png')
+  pfp = Image.open('collections/' + collection + '/clean_pfps/' + str(pfp_id) + '.png')
+  outfit = Image.open('collections/' + collection + '/outfits/' + outfit + '/' + fit + '.png')
 
-# This combines the images 
+  pfp.paste(outfit, (0, 0), mask=outfit)
+  pfp.save(save_file_path)
 
-    pfp = Image.open(pfp_folder + str(pfp_id) + '.png')
-    outfit = Image.open(outfits_folder + fit + '.png')
+  return save_file_path
 
-    pfp.paste(outfit, (0, 0), mask=outfit)
-    pfp.save(save_img_folder + 'dressed' + str(pfp_id) + '.png')
+@bot.command(name="gib", brief='Gib me cool pfp', description='This command will let you be much cooler than you are... fr fr')
+async def gib(ctx, collection: str, pfp_id: int, campaign: str, fit: typing.Optional[str] = 'default' ):
+  outfit = campaign
+  collection = collection.lower()
 
-    return
-    
-def get_bussin(fit, pfp_id):
-    url = ('https://degenape.nyc3.digitaloceanspaces.com/apes/no-head-traits/' + str(pfp_id) + '.png')
-    download_image(url, pfp_folder + str(pfp_id) + '.png')
+  try:
+    if collection in collections:
+      if collection == 'ape':
+        get_daa_image(pfp_id)
+      else:
+        get_collection_image(collection, pfp_id)
+    else: 
+      await ctx.send('Please enter a valid collection. ape / dtp / egg')
 
-# This combines the images 
+    save_file_path = combine_images(collection, outfit, fit, pfp_id)
 
-    pfp = Image.open(pfp_folder + str(pfp_id) + '.png')
-    outfit = Image.open(outfits_bussin_folder + fit + '.png')
+    await ctx.send(file=discord.File(save_file_path))
+  except:
+    await ctx.send('Please enter valid options for your request.\n\nTry `!gib-help` for more info')
 
-    pfp.paste(outfit, (0, 0), mask=outfit)
-    pfp.save(save_bussin_img_folder + 'dressed' + str(pfp_id) + '.png')
 
-    return
+@bot.command(name='gib-help', brief='List avaiable fits', description='This command will list the different outfits available to you')
+async def gib_help(ctx):
+    message =  """**Formatting the command examples**
 
-def get_jersey_dressed(pfp_id):
-    url = ('https://degenape.nyc3.digitaloceanspaces.com/apes/jersey-1/' + str(pfp_id) + '.png')
-    download_image(url, save_jersey_img_folder + str(pfp_id) + '.png')
+```md
+!gib [collection] [id] [campaign] [variant]
+!gib [ape/dtp/egg] [123] [solcap/solsnap/bussin/beer] [black/blue/red/white... etc]
 
-    return
+!gib ape 123 solcap
+!gib ape 123 bussin blue
+!gib dtp 456 solsnap 
+!gib egg 789 beer 
+```
 
-def get_dtp_dressed(fit, pfp_id):
-    
-    payload = json.dumps({
-        'condition': {
-            'project_ids': [
-                {
-                    'project_id': 'degentrashpandas'
-                }
-            ],
-            'name': {
-                'operation': 'EXACT', 
-                'value': 'Degen Trash Panda #' + str(pfp_id)
-                },
-        }
-    })
-    headers = {
-        'Authorization': os.environ['HYPER_TOKEN'],
-        'Content-Type': 'application/json'
-    }
-    r = requests.post('https://beta.api.solanalysis.com/rest/get-market-place-snapshots', headers=headers, data=payload)
-    
-    url = r.json()['market_place_snapshots'][0]['meta_data_img']
+==========================
 
-    download_image(url, pfp_dtp_folder + str(pfp_id) + '.png')
+**What I Can Gib**
 
-# This combines the images 
+**Beer**
+*collections:* `ape`, `dtp`,`egg`
+*variants:* `clean`, `background`
 
-    pfp = Image.open(pfp_dtp_folder + str(pfp_id) + '.png')
-    outfit = Image.open(outfits_dtp_folder + fit + '.png')
+**bussin**
+*collections:* `ape`, `dtp`
+*variants:* `black`, `blue`, `brown`, `gold`, `green`, `purple`, `red`, `white`
 
-    pfp.paste(outfit, (0, 0), mask=outfit)
-    pfp.save(save_dtp_img_folder + 'dressed' + str(pfp_id) + '.png')
+**solcap**
+*collections:* `ape`, `dtp`
 
-    return
+**solsnap**
+*collections:* `ape`, `dtp`
+    """
+    await ctx.send(message)
 
-def get_bussin_dtp(fit, pfp_id):
-    
-    payload = json.dumps({
-        'condition': {
-            'project_ids': [
-                {
-                    'project_id': 'degentrashpandas'
-                }
-            ],
-            'name': {
-                'operation': 'EXACT', 
-                'value': 'Degen Trash Panda #' + str(pfp_id)
-                },
-        }
-    })
-    headers = {
-        'Authorization': os.environ['HYPER_TOKEN'],
-        'Content-Type': 'application/json'
-    }
-    r = requests.post('https://beta.api.solanalysis.com/rest/get-market-place-snapshots', headers=headers, data=payload)
-    
-    url = r.json()['market_place_snapshots'][0]['meta_data_img']
 
-    download_image(url, pfp_dtp_folder + str(pfp_id) + '.png')
-
-# This combines the images 
-
-    pfp = Image.open(pfp_dtp_folder + str(pfp_id) + '.png')
-    outfit = Image.open(outfits_dtp_bussin_folder + fit + '.png')
-
-    pfp.paste(outfit, (0, 0), mask=outfit)
-    pfp.save(save_dtp_img_folder + 'bussin' + str(pfp_id) + '.png')
-
-    return
-
-def get_egg_dressed(fit, pfp_id):
-    
-    payload = json.dumps({
-        'condition': {
-            'project_ids': [
-                {
-                    'project_id': 'degenerateapekindergarten'
-                }
-            ],
-            'name': {
-                'operation': 'EXACT', 
-                'value': 'Degen Egg #' + str(pfp_id)
-                },
-        }
-    })
-    headers = {
-        'Authorization': os.environ['HYPER_TOKEN'],
-        'Content-Type': 'application/json'
-    }
-    r = requests.post('https://beta.api.solanalysis.com/rest/get-market-place-snapshots', headers=headers, data=payload)
-    
-    url = r.json()['market_place_snapshots'][0]['meta_data_img']
-
-    download_image(url, pfp_egg_folder + str(pfp_id) + '.png')
-
-# This combines the images 
-
-    pfp = Image.open(pfp_egg_folder + str(pfp_id) + '.png')
-    outfit = Image.open(outfits_egg_folder + fit + '.png')
-
-    pfp.paste(outfit, (0, 0), mask=outfit)
-    pfp.save(save_egg_img_folder + 'dressed' + str(pfp_id) + '.png')
-
-    return
-
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-
-# !beerme command executes the get_dressed function and returns the resulting image. It accepts a value between 1 and 10000. Update this to use the command name you want and the values to fit the range of your project
-
-@bot.command(name="beerme", brief='Add a beer helmet to your pfp', description='This command will let you apply new fits to your pfp')
-async def beerme(ctx, pfp_id: int, fit: typing.Optional[str] = "clean"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 10000:
-            get_dressed(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 10000.')
-
-@bot.command(name="beerme-ape", brief='Add a beer helmet to your pfp', description='This command will let you apply new fits to your pfp')
-async def beerme_ape(ctx, pfp_id: int, fit: typing.Optional[str] = "clean"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 10000:
-            get_dressed(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 10000.')
-
-@bot.command(name="seven", brief='Add a jersey to your pfp', description='This command will let you apply new fits to your pfp')
-async def seven(ctx, pfp_id: int):
-    try:
-        if 0 <= pfp_id <= 10000:
-            get_jersey_dressed(str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_jersey_img_folder + str(pfp_id) +'.png'))
-    except:
-        await ctx.send('Please enter a valid number between 1 and 10000.')
-
-@bot.command(name="beerme-panda", brief='Add a beer helmet to your trashy Panda', description='This command will let you add a beer helmet to your DTP')
-async def beer_panda(ctx, pfp_id: int, fit: typing.Optional[str] = "clean"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 20000:
-            get_dtp_dressed(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_dtp_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 20000.')
-
-@bot.command(name="beer-panda", brief='Add a beer helmet to your trashy Panda', description='This command will let you add a beer helmet to your DTP')
-async def beer_panda(ctx, pfp_id: int, fit: typing.Optional[str] = "clean"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 20000:
-            get_dtp_dressed(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_dtp_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 20000.')
-
-@bot.command(name="beerme-egg", brief='Add a beer helmet to your lovely Egg', description='This command will let you add a beer helmet to your DEGG')
-async def beer_egg(ctx, pfp_id: int, fit: typing.Optional[str] = "clean"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 2500:
-            get_egg_dressed(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_egg_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 2500.')
-
-@bot.command(name="bussin", brief='You bussin fr fr no cap', description='This command will let you be much cooler than you are')
-async def bussin_ape(ctx, pfp_id: int, fit: typing.Optional[str] = "black"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 10000:
-            get_bussin(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_bussin_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 10000.')
-
-@bot.command(name="bussin-panda", brief='You bussin fr fr no cap panda', description='This command will let you be much cooler than you are')
-async def bussin_dtp(ctx, pfp_id: int, fit: typing.Optional[str] = "black"):
-    try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 20000:
-            get_bussin_dtp(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_dtp_img_folder + 'bussin' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
-    except:
-        await ctx.send('Please enter a valid number between 1 and 10000.')
-
-# Lists the different "fits" available. This just returns the outfits list on new lines 
-
-@bot.command(brief='List avaiable fits', description='This command will list the different outfits available to you')
-async def fits(ctx):
-    await ctx.send('**List of Fits (please choose from one of the below)**\n\n'+ "\n".join(outfits))
-
-# Lets user know when they enter an invalid command
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound): # or discord.ext.commands.errors.CommandNotFound as you wrote
-        await ctx.send("Unknown command, please check !help for a list of available commands")
 
 load_dotenv()
 
 bot.run(os.environ['DISCORD_TOKEN'])
-
